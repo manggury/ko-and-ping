39424faba4e7a9ca75d25db6b4a2510186898c9b
manggury <148739312+manggury@users.noreply.github.com>
2024-02-02 16:38:55 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -4,6 +4,15 @@ $(function () {
         containment: '.visual_wrap',
         showControls: false,
         mute: true,
+        // responsive: [
+        //     {
+        //         breakpoint: 768,
+        //         settings: {
+
+        //             optimizeDisplay: true,
+        //         }
+        //     }
+        // ]
     })
 
 
@@ -15,12 +24,30 @@ $(function () {
         prevArrow: $('.arrow .left'),
         responsive: [
             {
-                breakpoint: 800, // 600px 이하에서 적용
+                breakpoint: 800, //
+                settings: {
+                    slidesToShow: 3
+                }
+
+            },
+            {
+                breakpoint: 600, // 600px 이하에서 적용
                 settings: {
                     slidesToShow: 2
                 }
+            },
+            {
+                breakpoint: 400, // 600px 이하에서 적용
+                settings: {
+                    slidesToShow: 1
+                }
             }
             // 추가적인 미디어 쿼리 설정 가능
-        ]
+        ],
+
     });
+    $('.mopen').on('click', function () {
+        $('.gnb').toggleClass('on');
+        $(this).find('.hamburger').toggleClass('is-active');
+    })
 });
